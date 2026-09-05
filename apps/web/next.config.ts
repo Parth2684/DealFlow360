f14fa0670d8be5a1dbf8facd4244675a261b9159
Karna <karnabhosle223@gmail.com>
2026-09-05 15:11:08 +0530

    update: tsconfig nextconfig

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.API_URL ?? "http://localhost:3000/api/v1";
+
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${API_URL}/:path*`,
+      },
+    ];
   },
 };
 
